261d78b109e2b19ecd3e7b7ae9b9ec95afdba0b2
Kohei Tsuyuki <tsuyuki.k@note.jp>
2023-08-31 15:40:17 +0900

    画像の表示をきれいにする

--- image.py
+++ image.py
@@ -1,19 +1,31 @@
 import openai
 import streamlit as st
+from more_itertools import chunked
 
 openai_api_key = st.secrets['openai']['api_key']
 openai.api_key = openai_api_key
 
+
+def define_cols(size: str) -> int:
+    if size == "256x256":
+        return 4
+    elif size == "512x512":
+        return 2
+    else:
+        return 1
+
 def generate_images(*args, **kwargs):
     size = kwargs.get("size", "256x256")
+    cols = define_cols(size)
     num = kwargs.get("num", 1)
     response = openai.Image.create(
         prompt=kwargs.get("prompt", "a white siamese cat"),
         n=num,
         size=size,
     )
-    for img in response["data"]:
-        st.image(img["url"])
+    for row in chunked(response["data"], cols):
+        for img, col in zip(row, st.columns(cols)):
+            col.image(img["url"])
 
 
 with st.sidebar:
